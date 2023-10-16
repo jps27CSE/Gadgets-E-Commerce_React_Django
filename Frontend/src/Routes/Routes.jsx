9d72b693ef2917866337703f94167c3387c266be
frs11~ <fayezurrahman017@gmail.com>
2023-10-16 20:52:21 +0600

    Added error Page

--- Frontend/src/Routes/Routes.jsx
+++ Frontend/src/Routes/Routes.jsx
@@ -3,11 +3,13 @@ import Layout from "../Layout/Layout";
 import Homepage from "../Pages/Homepage";
 import Login from "../Pages/Login";
 import Products from "../Pages/Products/Products";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 
 const customRoutes = createBrowserRouter([
   {
     path: "/",
     element: <Layout></Layout>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         path: "/",
